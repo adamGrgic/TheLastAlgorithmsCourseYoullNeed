622dc376518891626f3691f8be0bcf45a1f5b260
Adam Grgić <adam@grgicconsulting.com>
2024-07-15 20:09:50 -0600

    init

---
+++ scratch/src/main.ts
@@ -0,0 +1,62 @@
+// Notes
+// Why learn? 
+
+// - Learn the handshake!
+// - Will you use it on the job? Sometimes
+// - Its there and you don't have to be a genius to understand these things
+
+
+
+// Time and Space Complexity
+
+// Big O 
+// Categorizes your algorithm's time or memory requirements based on input 
+// Not an exact measurement
+// Will not tell you how many CPU cycles it takes
+// Generalize the growth of your algorithm
+
+
+// Great for creating performant applications which is highly desirable for business
+
+// Most Important Concepts
+// 1. Growth is with respect to input
+// 2. Constants are dropped
+// 3. Worst case is usually the way we measure 
+
+
+// common list of complexities
+// O(1)
+// O(logn)
+// O(n)
+// O(nlogn)
+// O(n^2)
+// O(2^n)
+// O(n!)
+
+// O(n log n)  -> Quicksort
+// O(log n) -> Binary search trees
+
+
+
+// arrays => simple contiguous memory space 
+
+
+// Sample array buffer things
+// const a = new ArrayBuffer(6);
+// console.log('a : ',a);
+
+// const a8 = new Uint8Array(a);
+
+// console.log('a8 at decleration: ', a8);
+
+// a8[0] = 45;
+
+// console.log('a8 after modifying a8[0] with 45', a8);
+
+// console.log('end');
+
+
+// Linear Search
+// *** It is good to be able to explain these things in your own words,
+// *** fluently, on a whiteboard :D 
+
